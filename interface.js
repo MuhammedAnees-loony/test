d84e2542453f1f2be9788a632bff89a2104afb8d
Mathews Reji <149689917+Mathews-Reji@users.noreply.github.com>
2024-07-07 22:46:30 +0530

    Update interface.js

--- interface.js
+++ interface.js
@@ -118,7 +118,6 @@ function enableTabs() {
         registerForm.style.display = 'none';
         document.getElementById('userProfile').style.display = 'none';
         document.getElementById('statusContent').style.display = 'none';
-        document.getElementById('keyFeatures').style.display = 'none';
     });
     
     // Registration form link
@@ -129,7 +128,6 @@ function enableTabs() {
         registerForm.style.display = 'block';
         document.getElementById('userProfile').style.display = 'none';
         document.getElementById('statusContent').style.display = 'none';
-        document.getElementById('keyFeatures').style.display = 'none';
     });
     
     // Default state on load
@@ -137,7 +135,6 @@ function enableTabs() {
     registerForm.style.display = 'none';
     document.getElementById('userProfile').style.display = 'none';
     document.getElementById('statusContent').style.display = 'none';
-    document.getElementById('keyFeatures').style.display = 'none';
 }
 
 // Event listener for login form submission
