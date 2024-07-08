3733164fdc61a2a7941caae0fa460c04f2905ebd
Mathews Reji <149689917+Mathews-Reji@users.noreply.github.com>
2024-07-09 01:49:20 +0530

    Update interface.js

--- interface.js
+++ interface.js
@@ -209,8 +209,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Create an iframe element to load the map HTML
         var iframe = document.createElement('iframe');
-        iframe.style.width = '100%';
-        iframe.style.height = '600px';
+        iframe.style.width = '330px';
+        iframe.style.height = '400px';
         iframe.style.border = 'none';
         iframe.srcdoc = mapHtml; // Embed the map HTML directly into the iframe
 
